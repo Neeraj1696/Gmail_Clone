46068326620c3ba8dbd2b1c1df49771ec357bb20
Neeraj Yadav <neeraj0yadav0@gmail.com>
2022-06-16 18:07:20 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 
 import "./App.css";
 import Header from "./Pages/Header";
+import Sidebar from "./Pages/Sidebar";
 
 function App() {
   return (
     <div className="app">
       {/* Header */}
       <Header />
+      {/* sidebar */}
+      <Sidebar />
     </div>
   );
 }
